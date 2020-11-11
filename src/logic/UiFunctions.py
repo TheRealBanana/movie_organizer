34fbbf0c0d0a50148c25e80acfedc34578fde5cc
Kyle Claisse <TheRealBanana@users.noreply.github.com>
2020-11-11 03:14:31 -0800

    Display data
    Display all the data for each title in a crude fashion. Will make it look nice later.
    
    It takes a long time to pull data for each title through the web api. I am going to investigate using the downloadable S3 datasets. Then we can just grab missing data online. Although if the downloaded data is missing even a single field it isnt worth it. Pulling the data from the API includes all the fields, not just one single one (maybe it supports that? will read the docs). There's also the older and more completel pre-2018 datasets.
    
    searching is one of the next things to get working.

--- src/logic/UiFunctions.py
+++ src/logic/UiFunctions.py
@@ -99,24 +99,63 @@ class UIFunctions:
             listitem.setToolTip(str(d[11]))
             self.uiref.movieLibraryList.addItem(listitem)
 
-    def updateLibraryDisplay(self, newitem, olditem):
+    def updateLibraryDisplay(self, newitem, _):
         print("CHANGED")
         data = newitem.data(QtCore.Qt.UserRole)
         title = newitem.text()
         path = newitem.toolTip()
-        displaytext = """SELECTED_MOVIE_DATA: 
 
-TITLE:
-%s
+        #0,dbdata["title"]
+        #1,dbdata["directors"]
+        #2,dbdata["writers"]
+        #3,dbdata["producers"]
+        #4,dbdata["actors"]
+        #5,dbdata["composers"]
+        #6,dbdata["genres"]
+        #7,dbdata["runtime"]
+        #8,dbdata["coverurl"]
+        #9,dbdata["playcount"]
+        #10,dbdata["lastplay"]
+        #11,dbdata["filelocation"]
+        #12,dbdata["imdb_id"]
+        #13,dbdata["imdb_rating"]
+        #14,dbdata["rating"]
+        #15,dbdata["extra1"]
+        #16,dbdata["extra2"]
+        #Format data for display:
+        #most stuff is ok just the lists need to be adjusted
+        for i, d in enumerate(data):
+            if isinstance(d, list):
+                #Actors lists need special care
+                if isinstance(d[0], dict):
+                    data[i] = "<br>".join(["{name} as {character}".format(**r) for r in d])
+                else:
+                    data[i] = ", ".join(d)
 
-PATH:
-%s
+        displaytext = """SELECTED_MOVIE_DATA: <br><br>
 
-FULL_DATA:
-%s
+<b><h2>TITLE:</h2></b>  <h1>%s</h1> <br><br>
+<b>DIRECTORS:</b>  %s<br><br>
+<b>WRITERS:</b>  %s<br><br>
+<b>PRODUCERS:</b>  %s<br><br>
+<b>ACTORS:</b>  %s<br><br>
+<b>COMPOSERS:</b>  %s<br><br>
+<b>GENRES:</b>  %s<br><br>
+<b>RUNTIME:</b>  %s<br><br>
+<b>COVER_URL:</b>  %s<br><br>
+<b>PLAYCOUNT:</b>  %s<br><br>
+<b>LASTPLAY:</b>  %s<br><br>
+<b>FILELOCATION:</b>  %s<br><br>
+<b>IMDB_ID:</b>  %s<br><br>
+<b>IMDB_RATING:</b>  %s<br><br>
+<b>RATING:</b>  %s<br><br>
+<b>EXTRA1:</b>  %s<br><br>
+<b>EXTRA2:</b>  %s<br><br>
+
+
       
-""" % (title, path, str(data))
-        self.uiref.movieInfoDisplay.setText(displaytext)
+""" % (*data,)
+        self.uiref.movieInfoDisplay.setHtml(displaytext)
 
     def openOptionsDialog(self):
         dialog = QtWidgets.QDialog(self.MainWindow)

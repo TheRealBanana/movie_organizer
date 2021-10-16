a4d8203ce25c727cb993122db88fedf96557c5fc
Kyle Claisse <kyle@kylesplace.org>
2021-10-16 02:10:30 -0700

    More subtitles work
    Got the basic commands worked out for extracting subtitles from video files. At first I was using pymediainfo (MediaInfoDLL) but it uses so much data whereas mkvmerge is like 5x less data hungry. Extracting the subs themselves requires lots of data but thats normal. Shouldnt be run over slow networks. Currently ignores SDH subs but if I can get the filtering working it shouldnt matter if its SDH or normal subs.
    
    Really this code should be run as a QThread so will have to work that in somehow.

--- src/logic/subtitles.py
+++ src/logic/subtitles.py
@@ -1,11 +1,12 @@
 #All the subtitle related code here
-
-import subliminal, babelfish
+import json
+from subprocess import check_output
 from .movie_library import checkDbOpen, getDbCursor
 from dialogs.widgets.generic_progress_bar import genericProgressDialog
 
 import os, sqlite3
 
+SUBLANG = "eng"
 DATABASE_PATH = "./subtitles.db"
 
 class SubtitleLibrary:
@@ -72,10 +73,51 @@ class SubtitleLibrary:
                 self.subsdict[subsdata["title"]] = subsdata
 
 
+#This probably should be threaded but Im lazy and just want it working
 class SubtitleDownloader:
     def __init__(self, libraryref):
         self.libraryref = libraryref
-        subliminal.region.configure("dogpile.cache.memory")
+
 
     def updateCache(self):
         print("YEAH NO")
+
+    def getSubsFromFile(self):
+        # pymediainfo.MediaInfo.parse()
+        # Go over our library and check for entries that don't have any subs in the subtitles db
+        # Then use pymediainfo to check for subs and determine the correct one to pull
+
+        pass
+
+    def getSubtitleTrackIds(self, vidpath):
+        #Using mkvmerge's json output we can look for the exact track we want and then extract it
+        cmd = "mkvmerge --identification-format json --identify \"%s\"" % vidpath
+        videodata = json.loads(check_output(cmd, shell=True))
+        goodsubids = [s["id"] for s in videodata["tracks"] if s["type"] == "subtitles" and \
+                      s["properties"]["language"] == SUBLANG and \
+                      s["properties"]["forced_track"] is False and \
+                      "S_TEXT" in s["properties"]["codec_id"] and \
+                      ("SDH" not in s["properties"]["track_name"].upper() if "track_name" in s["properties"] else True)] # Would prefer not to use SDH subs but maybe I can filter out actions
+        #At this point its hoped that there is only one track
+        #If theres more we'll investigate later
+        if len(goodsubids) > 1:
+            print("God more than 1 good track for %s" % vidpath)
+        return goodsubids
+
+
+    #It seems that practically speaking you have to read the entire file to
+    #extract any given subtitle track. So don't run this over slow network paths.
+    def extractSubtitlesFromVideo(self, vidpath, trackid):
+        cmd = "mkvextract tracks %s %s:subsout.txt" % (vidpath, trackid)
+        os.system(cmd)
+        with open("subsout.txt", "r") as subsfile:
+            subtitles = subsfile.read()
+        return subtitles
+
+
+    #In addition to time codes there are many subtitles are are representative of actions and not dialog.
+    #This function aims to remove actions and possibly ascribe dialog to individual characters.
+    #Some subtitles seem to contain information about the speaker but this may not be universal.
+    #For now just returning dialog alone is sufficient for our needs.
+    def filterDialogFromSubs(self, subtitles):
+        pass
